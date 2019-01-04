890016aeb6a635819f3d73d416e29b5d347ef36f
Kabir <kabir_ub@ymail.com>
2019-01-03 21:59:41 -0500

    scanner closed

--- src/chpterThreeDecision/softwareSales8.java
+++ src/chpterThreeDecision/softwareSales8.java
@@ -1,9 +1,37 @@
 package chpterThreeDecision;
 
+import java.util.Scanner;
+
 public class softwareSales8 {
 
 	public static void main(String[] args) {
+		int numberOfPackage;
+		double discountAmount;
+		double totalAmountPurchase;
 		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println(" please enter the number of package purchase");
+		numberOfPackage = keyboard.nextInt();
+		
+		if(numberOfPackage>=10 && numberOfPackage<19) {
+			discountAmount=(numberOfPackage*99)*.20;	
+		}
+		else if(numberOfPackage>=20 && numberOfPackage<49) {
+			discountAmount=(numberOfPackage*99)*.30;	
+		}
+		else if(numberOfPackage>=50 && numberOfPackage<99) {
+			discountAmount=(numberOfPackage*99)*.40;	
+		}
+		else if(numberOfPackage>=100) {
+			discountAmount=(numberOfPackage*99)*.50;	
+		}	
+		
+		
+		
+		keyboard.close();
 	}
+	
+	
 
 }
